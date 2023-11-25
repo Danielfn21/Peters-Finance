601f97cba721f232f4f640ccb9d73e9df12f4dfc
OFHansen <ohans21@student.sdu.dk>
2023-11-25 03:13:09 +0100

    topBar works

--- frontend/app/src/main/java/com/example/peters_finance/groups/GroupChat.kt
+++ frontend/app/src/main/java/com/example/peters_finance/groups/GroupChat.kt
@@ -1,5 +1,6 @@
 package com.example.peters_finance.groups
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,42 +9,86 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 @Preview
 @Composable
 fun GroupChat() {
 
-    Column(
+    Surface(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-        ){
+    ) {
+        TopBar()
+    }
+}
 
-        //TODO: Change this to topbar
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(45.dp)
-                    .background(color = Color.Gray)
-                    .padding(15.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text = "←")
-                Text(text = "Group Name")
-                Text(text = "⚙")
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Gray
+                ),
+                title = {
+                    Text(text = "Chat")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            //TODO: nav back
+                        }
+                    ) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            //TODO: nav settings page
+                        }
+                    ) {
+                        Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+                    }
+                }
+            )
+        },
+
+        ) {
+        Column (
+            modifier = Modifier.paddingFromBaseline(top = 15.dp).padding(20.dp)
+        ){
+            Chat()
         }
     }
 }
 
 @Composable
-fun Chat(){
+fun Chat() {
     //https://medium.com/@meytataliti/building-a-simple-chat-app-with-jetpack-compose-883a240592d4
 }
