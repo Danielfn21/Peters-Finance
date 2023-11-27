2996dd1f533ab46f8fe0d936aedcf83d0d983efe
OFHansen <ohans21@student.sdu.dk>
2023-11-27 02:02:08 +0100

    Cleaned up MainActivity by moving navigation to its own file

--- frontend/app/src/main/java/com/example/peters_finance/MainActivity.kt
+++ frontend/app/src/main/java/com/example/peters_finance/MainActivity.kt
@@ -3,19 +3,7 @@ package com.example.peters_finance
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.peters_finance.api.fetchUsers
-import com.example.peters_finance.groups.GroupHomeOverview
-import com.example.peters_finance.login.AccountPage
-import com.example.peters_finance.login.CreateAccountPage
-import com.example.peters_finance.login.LoginPage
-import com.example.peters_finance.login.SplashScreen
-import com.example.peters_finance.models.User
+import com.example.peters_finance.navigation.AppNavigation
 
 
 class MainActivity : ComponentActivity() {
@@ -23,44 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            var currentUser = remember{ mutableStateOf<User?>(null) }
-
-            //Grab all users stored in the database
-            var allUsers = emptyList<User>()
-            LaunchedEffect(true){
-                allUsers = fetchUsers()
-            }
-
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "splash") {
-                composable("splash") {
-                    SplashScreen(navController)
-                }
-
-                composable("LoginPage") {
-                    LoginPage(navController, allUsers, fetchUser = { fetchUser ->
-                        currentUser.value = fetchUser
-                    })
-                }
-
-                composable("CreateAccountPage") {
-                    CreateAccountPage(navController, newUser = { newUser ->
-                        currentUser.value = newUser
-                    })
-                }
-
-                composable("GroupHomeOverview") {
-                    GroupHomeOverview(navController, currentUser.value)
-                }
-
-                composable("AccountPage") {
-                    AccountPage(navController, currentUser.value)
-                }
-
-            }
-
-
+            AppNavigation()
         }
     }
 }
---
+++ frontend/app/src/main/java/com/example/peters_finance/navigation/AppNavigation.kt
@@ -0,0 +1,56 @@
+package com.example.peters_finance.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.peters_finance.api.fetchUsers
+import com.example.peters_finance.groups.GroupHomeOverview
+import com.example.peters_finance.login.AccountPage
+import com.example.peters_finance.login.CreateAccountPage
+import com.example.peters_finance.login.LoginPage
+import com.example.peters_finance.login.SplashScreen
+import com.example.peters_finance.models.User
+
+@Composable
+fun AppNavigation() {
+    var currentUser = remember{ mutableStateOf<User?>(null) }
+
+    //Grab all users stored in the database
+    var allUsers = emptyList<User>()
+    LaunchedEffect(true){
+        allUsers = fetchUsers()
+    }
+
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("splash") {
+            SplashScreen(navController)
+        }
+
+        composable("LoginPage") {
+            LoginPage(navController, allUsers, fetchUser = { fetchUser ->
+                currentUser.value = fetchUser
+            })
+        }
+
+        composable("CreateAccountPage") {
+            CreateAccountPage(navController, newUser = { newUser ->
+                currentUser.value = newUser
+            })
+        }
+
+        composable("GroupHomeOverview") {
+            GroupHomeOverview(navController, currentUser.value)
+        }
+
+        composable("AccountPage") {
+            AccountPage(navController, currentUser.value)
+        }
+
+    }
+
+}
